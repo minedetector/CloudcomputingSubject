eca7714778a8a10ceb76647f4375d4e945997041
ssalum <ssalum@ttu.ee>
2021-01-25 22:29:39 +0200

    Parandasin salasõna loomist ja lõin emaili saatmiseks exceli loomise

--- User management/user-creation.py
+++ User management/user-creation.py
@@ -1,4 +1,11 @@
 import csv
+import xlsxwriter
+import random
+
+workbook = xlsxwriter.Workbook('send-info.xlsx') 
+worksheet = workbook.add_worksheet()
+worksheet_row = 0
+worksheet_column = 0
 
 creation_file = open('user-creation-commands.txt', 'a')
 deletion_file = open('user-deletion-commands.txt', 'a')
@@ -10,11 +17,14 @@ with open('tudengid.csv') as csv_file:
             line_count += 1
         else:
             username = row[0] + row[1]
+            email = row[2]
+            pw_nr = random.randint(1,99999)
+            password = username + str(pw_nr)
             group = 'Tudengid'
             arn = 'arn:aws:iam::311962906195:policy/Tudengite-oigused'
 
             create_user = (f'aws iam create-user --user-name {username}\n')
-            create_user_pw = (f"aws iam create-login-profile --user-name {username} --password '{username}1!' --password-reset-required\n")
+            create_user_pw = (f"aws iam create-login-profile --user-name {username} --password '{password}' --password-reset-required\n")
             add_user_to_group = (f'aws iam add-user-to-group --user-name {username} --group-name  {group}\n')
 
             creation_file.write(create_user)
@@ -29,7 +39,13 @@ with open('tudengid.csv') as csv_file:
             deletion_file.write(delete_user_login_profile)
             deletion_file.write(delete_user)
 
+            worksheet.write(line_count, 0, email)
+            worksheet.write(line_count, 1, username + '; ' + password)
+
             line_count += 1
+
     print(f'Loodi {line_count - 1} kasutajat.')
 creation_file.close()
 deletion_file.close()
+workbook.close()
+
